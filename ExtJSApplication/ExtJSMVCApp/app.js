4a3849cf5ad58870e0630e6a15223323380039ec
Akhil <akhil.munna02@gmail.com>
2016-01-20 06:44:47 +0530

    Started Building app in MVC, index.html is starting file, app.js is mainview and sessions.js is view to load sessions

---
+++ ExtJSApplication/ExtJSMVCApp/app.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="C:\Users\Naruto\Source\Repos\ExtJSApplication\ExtJSApplication\ExtJS/ext-all-debug.js" />
+
+Ext.Loader.setConfig({
+    enabled: true
+});
+
+Ext.application({
+    name: 'SE',
+    requires: ['SE.view.MainView'],
+    views:['Sessions'],
+    launch: function () {
+        Ext.create('SE.view.MainView');
+
+    }
+});
---
+++ ExtJSApplication/app/view/Sessions.js
@@ -0,0 +1,96 @@
+﻿/// <reference path="C:\Users\Naruto\Source\Repos\ExtJSApplication\ExtJSApplication\ExtJS/ext-all-debug.js" />
+
+
+Ext.define('SE.view.Sessions', {
+    extend: 'Ext.grid.Panel',
+    alias: 'widget.sessiongridpanel',
+
+    listeners: {
+        itemdblclick: function (gridpanel, record, item, e) {
+            var formWindow = Ext.create('SessionEditForm');
+            var form = formWindow.down('form');
+            form.loadRecord(record)
+            formWindow.show();
+        }
+
+    },
+
+    store: {
+        fields: ['id',
+            {
+                name: 'title', sortType: 'asUCText'
+            },
+            'approved',
+            {
+                dateFormat: 'c',
+                name: 'sessionTimeDateTime',
+                sortType: 'asDate',
+                type: 'date'
+            },
+            {
+                convert: function (v, rec) {
+                    var convertIt = Ext.util.Format.dateRenderer('m/d/Y g:i a'),
+                        pretty = convertIt(rec.get('sessionTimeDateTime'));
+                    return pretty;
+
+                },
+                name: 'sessionTimePretty',
+                type: 'string'
+
+            }
+
+
+        ],
+        autoLoad: true,
+        autoSync: true,
+        proxy: {
+            type: 'rest',
+            url: '/data/sessions.json',
+            reader: {
+                type: 'json',
+                root: 'data'
+            }
+        },
+        sorters: [
+            { property: 'title' }
+        ],
+
+        groupField: 'sessionTimeDateTime'
+    },
+
+    columns: [
+           {
+               xtype: 'gridcolumn',
+               dataIndex: 'id',
+               text: 'Id'
+           },
+           {
+               xtype: 'gridcolumn',
+               dataIndex: 'title',
+               text: 'Title',
+               flex: 1,
+               minWidth: 100,
+               width: 75
+           },
+           {
+               xtype: 'gridcolumn',
+               dataIndex: 'approved',
+               text: 'Approved'
+           },
+           {
+               dataIndex: 'sessionTimePretty',
+               text: 'Session Start Time',
+               width: 100
+
+           }
+    ],
+
+    features: [
+        {
+            ftype: 'grouping',
+            groupHeaderTpl: [
+                   '{[values.rows[0].get(\'sessionTimePretty\')]} (Session Count: {rows.length})'
+            ]
+        }
+    ]
+});
